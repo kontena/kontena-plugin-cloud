86a9d47c0c2b8fac4c4a99cbe74427f65766d587
Lauri Nevala <lauri.nevala@gmail.com>
2017-08-13 16:00:03 +0300

    Update help texts

--- lib/kontena/plugin/platform/user_command.rb
+++ lib/kontena/plugin/platform/user_command.rb
@@ -5,9 +5,9 @@ require_relative 'user/add_command'
 require_relative 'user/remove_command'
 class Kontena::Plugin::Platform::UserCommand < Kontena::Command
   include Kontena::Plugin::Platform
-  subcommand ['list', 'ls'], 'List organizations', User::ListCommand
-  subcommand 'add', 'Add users to organization', User::AddCommand
-  subcommand ['remove', 'rm'], 'Remove users from organization', User::RemoveCommand
+  subcommand ['list', 'ls'], 'List platform users', User::ListCommand
+  subcommand 'add', 'Add users to platform', User::AddCommand
+  subcommand ['remove', 'rm'], 'Remove users from platform', User::RemoveCommand
 
   def execute
   end
