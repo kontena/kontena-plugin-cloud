fdd7d347c242d65560a1815d857be1ab1e79319d
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-08-30 09:45:41 +0300

    fix load path

--- lib/kontena/plugin/cloud/platform_command.rb
+++ lib/kontena/plugin/cloud/platform_command.rb
@@ -4,7 +4,7 @@ class Kontena::Plugin::Cloud::PlatformCommand < Kontena::Command
   subcommand 'show', 'Show platform details', load_subcommand('kontena/plugin/cloud/platform/show_command')
   subcommand 'create', 'Create new platform', load_subcommand('kontena/plugin/cloud/platform/create_command')
   subcommand ['remove', 'rm'], 'Remove platform', load_subcommand('kontena/plugin/cloud/platform/remove_command')
-  subcommand ['join', 'byo'], 'Join grid as Kontena Platform', load_subcommand('kontena/plugin/platform/import_grid_command')
+  subcommand ['join', 'byo'], 'Join grid as Kontena Platform', load_subcommand('kontena/plugin/cloud/platform/import_grid_command')
   subcommand 'user', 'User management commands', load_subcommand('kontena/plugin/cloud/platform/user_command')
 
   def execute
