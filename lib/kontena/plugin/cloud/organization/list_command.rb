88b93e652cbd160a880e7bc86200f3d3ba2a608c
Lauri Nevala <lauri.nevala@gmail.com>
2017-10-15 14:22:05 +0300

    Refactor organization list command

--- lib/kontena/plugin/cloud/organization/list_command.rb
+++ lib/kontena/plugin/cloud/organization/list_command.rb
@@ -1,12 +1,14 @@
+require_relative 'common'
 class Kontena::Plugin::Cloud::Organization::ListCommand < Kontena::Command
   include Kontena::Cli::Common
+  include Kontena::Plugin::Cloud::Organization::Common
   include Kontena::Cli::TableGenerator::Helper
 
   requires_current_account_token
 
   def execute
-    organizations = cloud_client.get('/organizations')['data']
-    print_table(organizations) do |row|
+    organizations = fetch_organizations
+    print_table(organizations.map{|o| o.api_data}) do |row|
       row.merge!(row['attributes'])
       row['name'] = row['name']
       row['account-status'] = row['account-status'] == 'active' ? pastel.green(row['account-status']) : row['account-status']
