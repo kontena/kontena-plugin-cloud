ffac5b209ac0f114b94d34b8c27241d3b6b8f7cf
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-10-17 15:42:04 +0300

    fix platform upgrade

--- lib/kontena/plugin/cloud/platform/upgrade_command.rb
+++ lib/kontena/plugin/cloud/platform/upgrade_command.rb
@@ -13,32 +13,39 @@ class Kontena::Plugin::Cloud::Platform::UpgradeCommand < Kontena::Command
   def execute
     require_platform(name)
 
+    to_version = self.version
     platform = find_platform_by_name(current_platform, current_organization)
-    unless version
-      version = prompt_version(platform)
-      if Gem::Version.new(version) < Gem::Version.new(platform.version)
-        exit_with_error "Dowgrade is not supported"
+    unless to_version
+      to_version = prompt_version(platform)
+      if Gem::Version.new(to_version) < Gem::Version.new(platform.version)
+        exit_with_error "Downgrade is not supported"
       end
     end
     data = {
       attributes: {
         name: platform.name,
-        version: platform.version
+        version: to_version
       }
     }
-    cloud_client.put("/organizations/#{current_organization}/platforms/#{platform.name}", { data: data })
+    spinner "Upgrading platform #{pastel.cyan(name)} to version #{to_version}" do
+      cloud_client.put("/organizations/#{current_organization}/platforms/#{platform.name}", { data: data })
+      while platform.version != to_version do
+        sleep 5
+        platform = find_platform_by_name(platform.id, current_organization)
+      end
+    end
   end
 
-  # @param
+  # @param platform [Platform]
   def prompt_version(platform)
     versions = cloud_client.get("/platform_versions")['data']
 
     platform_version = Gem::Version.new(platform.version)
     versions = versions.select { |v| Gem::Version.new(v['id']) > platform_version }
 
-    default_version = versions.find_index{ |v| v['id'] == platform.version } + 1
+    exit_with_error "Platform is already on the latest version" if versions.size == 0
+
     prompt.select("Upgrade to version:") do |menu|
-      menu.default default_version
       versions.each do |v|
         menu.choice v['id']
       end
