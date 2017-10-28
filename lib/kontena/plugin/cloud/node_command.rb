faba39396a2ba5dd5ca0498c8aecf7180a5559ce
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-10-28 10:49:34 +0300

    node rm (alias for terminate)

--- lib/kontena/plugin/cloud/node_command.rb
+++ lib/kontena/plugin/cloud/node_command.rb
@@ -3,7 +3,7 @@ class Kontena::Plugin::Cloud::NodeCommand < Kontena::Command
   subcommand ['list', 'ls'], 'List cloud nodes', load_subcommand('kontena/plugin/cloud/node/list_command')
   subcommand ['shell'], 'Jump into node shell', load_subcommand('kontena/plugin/cloud/node/shell_command')
   subcommand ['create'], 'Create cloud node', load_subcommand('kontena/plugin/cloud/node/create_command')
-  subcommand ['terminate'], 'Terminate cloud node', load_subcommand('kontena/plugin/cloud/node/terminate_command')
+  subcommand ['terminate', 'rm'], 'Terminate cloud node', load_subcommand('kontena/plugin/cloud/node/terminate_command')
 
   def execute
   end
