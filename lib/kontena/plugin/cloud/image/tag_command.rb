2047033e235d61c33778962040058d954d52b765
Jussi Nummelin <jussi@kontena.io>
2017-11-07 20:24:32 +0200

    remove tag rm command for now

--- lib/kontena/plugin/cloud/image/tag_command.rb
+++ lib/kontena/plugin/cloud/image/tag_command.rb
@@ -1,7 +1,6 @@
 class Kontena::Plugin::Cloud::Image::TagCommand < Kontena::Command
 
     subcommand ['list', 'ls'], 'List image repository tags', load_subcommand('kontena/plugin/cloud/image/tag/list_command')
-    subcommand ['remove', 'rm'], 'Remove tags from image repositor', load_subcommand('kontena/plugin/cloud/image/tag/remove_command')
 
     def execute
     end
