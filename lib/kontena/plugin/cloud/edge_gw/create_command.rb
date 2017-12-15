2977bef76a8d9d01f5d6b66ca56d6fea56840daa
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-12-15 14:49:20 +0200

    wait dns propagation by default

--- lib/kontena/plugin/cloud/edge_gw/create_command.rb
+++ lib/kontena/plugin/cloud/edge_gw/create_command.rb
@@ -1,3 +1,4 @@
+require 'resolv'
 require 'kontena/machine/random_name'
 require_relative 'common'
 require_relative '../organization/common'
@@ -14,6 +15,7 @@ class Kontena::Plugin::Cloud::EdgeGw::CreateCommand < Kontena::Command
 
   parameter "[NAME]", "Cloud edge gateway name"
   option "--region", "REGION", "Region (us-east-1, eu-west-1, defaults to current platform region)"
+  option "--[no-]wait-dns", :flag, "Wait for edge gateway DNS to resolve", default: true
 
   def execute
     org, platform = parse_platform_name(current_platform)
@@ -36,8 +38,21 @@ class Kontena::Plugin::Cloud::EdgeGw::CreateCommand < Kontena::Command
         }
       }
     }
-    spinner "Provisioning an edge gateway #{pastel.cyan(name)} to platform #{pastel.cyan(platform.to_path)}, region #{pastel.cyan(target_region)}" do
+    response = spinner "Provisioning an edge gateway #{pastel.cyan(name)} to platform #{pastel.cyan(platform.to_path)}, region #{pastel.cyan(target_region)}" do
       compute_client.post("/organizations/#{current_organization}/edge-gateways", { data: data })
+    end
+    gw = Kontena::Cli::Models::EdgeGateway.new(response['data'])
+    if wait_dns?
+      resolver = Resolv::DNS.new(nameserver: ['8.8.8.8', '8.8.4.4'])
+      spinner "Waiting for edge gateway #{pastel.cyan(name)} dns #{pastel.cyan(gw.dns)} to resolve" do
+        sleep 60
+        while resolver.getaddresses(gw.dns).size == 0
+          sleep 5
+        end
+      end
+    else
+      puts ""
+      puts " Kontena Cloud Edge Gateway is now available at #{pastel.cyan(gw.dns)} (DNS propagation might take few minutes)"
     end
   end
 
