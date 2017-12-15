407209be44c3cbd9f3ff7b0c81a8e3549d9bd80f
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-12-15 14:30:49 +0200

    eg -> gw

--- lib/kontena/plugin/cloud_command.rb
+++ lib/kontena/plugin/cloud_command.rb
@@ -2,7 +2,7 @@ class Kontena::Cli::CloudCommand < Kontena::Command
 
   subcommand 'platform', 'Kontena platform specific commands', load_subcommand('kontena/plugin/cloud/platform_command')
   subcommand 'node', 'Kontena node specific commands', load_subcommand('kontena/plugin/cloud/node_command')
-  subcommand ['edge-gateway', 'eg'], 'Kontena edge gateway specific commands', load_subcommand('kontena/plugin/cloud/edge_gw_command')
+  subcommand ['edge-gateway', 'gw'], 'Kontena edge gateway specific commands', load_subcommand('kontena/plugin/cloud/edge_gw_command')
   subcommand ['organization', 'org'], 'Organization specific commands', load_subcommand('kontena/plugin/cloud/organization_command')
   subcommand ['image-repository', 'ir'], 'Image repository specific commands', load_subcommand('kontena/plugin/cloud/image_command')
   subcommand 'region', 'Region specific commands', load_subcommand('kontena/plugin/cloud/region_command')
